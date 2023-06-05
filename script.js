7f5cc167281051cbe44eb2c3386e8af7cecf3504
Antonio Llamas <antonio.llamas@thebridge.tech>
2023-06-05 09:28:53 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+// ITERACIÓN 1: VARIABLES
+
+// 1.1 Crea una variable developer1 y asignale un nombre cualquiera.
+
+// AQUÍ TU CÓDIGO
+
+// 1.2.Imprime en consola el texto "La primera desarrolladora se llama ****"
+
+// AQUÍ TU CÓDIGO
+
+// 1.3.Crea una variable developer2 y asigna otro nombre.
+
+// AQUÍ TU CÓDIGO
+
+// 1.4. Imprime en consola "El segundo desarrollador se llama ****"
+
+// AQUÍ TU CÓDIGO
+
+// ITERACIÓN 2: CONDICIONALES
+
+// 2.1 Dependiendo de qué desarrollador tenga el nombre más largo se debe imprimir en consola:
+
+// "El nombre **** es el más largo con ** caracteres".
+
+// Pero si ambos tienen el mismo número de caracteres entonces imprime:
+
+// "Qué casualidad, ambos nombres tienen ** caracteres".
+
+// AQUÍ TU CÓDIGO
+
+// ITERACIÓN 3: FUNCIONES Y BUCLES
+
+// Descomenta la siguiente linea de código.
+
+// let input = prompt("Escribe un nombre");
+
+// 3.1 Declara una función que acepte una palabra como parámetro y la devuelva con todos sus caracteres en mayúsculas y separados por espacios. Ejemplo: Para "Ana" devolverá "A N A".
+
+// AQUÍ TU CÓDIGO
+
+// 3.2 Declara una función que acepte un nombre como parámetro y devuelva el número de palabras que tiene el nombre. Ejemplo: Para "Antonio Alberto Jesús" devolverá 3.
+
+// AQUÍ TU CÓDIGO
+
+// 3.3 Declara una tercera función llamada printUserDetails que haga uso de las dos funciones anteriores y reciba la entrada del usuario en el prompt. La función debe imprimir en consola la siguiente salida:
+
+// Para la entrada "Antonio Alberto Jesús" la consola imprimirá:
+
+/*
+
+DETALLES para 'Antonio Alberto Jesus':
+Número de palabras 3:
+- A N T O N I O (7 caracteres)
+- A L B E R T O (7 caracteres)
+- J E S U S (5 caracteres) 
+
+*/
+
+// AQUÍ TU CÓDIGO
+
+// Happy coding!
